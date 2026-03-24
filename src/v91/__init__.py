5c9772e2cd1be25842ee6a283b277a52e6bee33f
guenny9000 <guenny9000@gmail.com>
2026-03-24 13:47:33 +0100

    Initialize the V91 module and import core components

---
+++ src/v91/__init__.py
@@ -0,0 +1,7 @@
+# V91 Module Initialization
+
+"""
+This module initializes the V91 package and imports core components.
+"""
+
+from .core import Component1, Component2  # Import the core components of the V91 module
